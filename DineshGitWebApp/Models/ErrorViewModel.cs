0d0ff6c0a80a3b91e79ca99960222e31ce4bbe6d
Dinesh R <thiru.dinesh@gmail.com>
2021-03-13 20:07:04 +0530

    First checkin

---
+++ DineshGitWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DineshGitWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
